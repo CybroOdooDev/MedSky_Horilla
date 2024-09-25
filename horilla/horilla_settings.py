03709b79777e014e4c1be8b5db864434b544a410
Nikhil <nikhil@cybrosys.in>
2024-09-25 10:52:59 +0200

    Initial commit

---
+++ horilla/horilla_settings.py
@@ -0,0 +1,31 @@
+from horilla import settings
+	
+"""
+DB_INIT_PASSWORD: str
+
+The password used for database setup and initialization. This password is a
+48-character alphanumeric string generated using a UUID to ensure high entropy and security.
+"""
+DB_INIT_PASSWORD = "60bYCPWabf4Z4dq53GbRvuSJLwPOlWAzOg4bELOTxjnWPOhHg3"
+
+
+HORILLA_DATE_FORMATS = {
+    "DD-MM-YYYY": "%d-%m-%Y",
+    "DD.MM.YYYY": "%d.%m.%Y",
+    "DD/MM/YYYY": "%d/%m/%Y",
+    "MM/DD/YYYY": "%m/%d/%Y",
+    "YYYY-MM-DD": "%Y-%m-%d",
+    "YYYY/MM/DD": "%Y/%m/%d",
+    "MMMM D, YYYY": "%B %d, %Y",
+    "DD MMMM, YYYY": "%d %B, %Y",
+    "MMM. D, YYYY": "%b. %d, %Y",
+    "D MMM. YYYY": "%d %b. %Y",
+    "dddd, MMMM D, YYYY": "%A, %B %d, %Y",
+}
+
+HORILLA_TIME_FORMATS = {
+    "hh:mm A": "%I:%M %p",  # 12-hour format
+    "HH:mm": "%H:%M",  # 24-hour format
+}
+
+BIO_DEVICE_THREADS = {}
